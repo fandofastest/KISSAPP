560f3ede99399b8ed54ead7998a980dce354fea8
fando <fandofast@gmail.com>
2020-02-05 19:44:58 +0700

    Initial commit

---
+++ app/src/main/java/com/kissapp/kissasian/cn_network/apis/AppConfigApi.java
@@ -0,0 +1,14 @@
+package com.kissapp.kissasian.cn_network.apis;
+
+import com.kissapp.kissasian.cn_network.model.AppConfig;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface AppConfigApi {
+
+    @GET("get_config")
+    Call<AppConfig> getAppConfig(@Query("api_secret_key") String apiKey);
+
+}
